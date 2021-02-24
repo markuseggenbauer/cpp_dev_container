2efe11ef7e028455d0809f8bb61a885ccc979dfd
Eggenbauer Markus <markus.eggenbauer@gmail.com>
2021-02-24 21:02:40 +0100

    Update of cpp_dojo to use conan for build_api

--- cpp_dojo/setup.cmake
+++ cpp_dojo/setup.cmake
@@ -1,24 +1,6 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
-set(CMAKE_CXX_FLAGS_DEBUG
-    "-g -O1 -fsanitize=undefined -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls"
-    CACHE STRING "Flags used by the C++ compiler during coverage builds." FORCE)
-
-# Get cmake hlper functions (later via conan, right now via FetchContent)
-include(FetchContent)
-FetchContent_Declare(
-  build_api
-  GIT_REPOSITORY https://github.com/markuseggenbauer/build_api.git
-  GIT_TAG main)
-
-FetchContent_GetProperties(build_api)
-if(NOT build_api_POPULATED)
-  FetchContent_Populate(build_api)
-endif()
-list(APPEND CMAKE_MODULE_PATH ${build_api_SOURCE_DIR}/cmake/lib/src)
-set(ME_CMAKE_SOURCE_DIR ${build_api_SOURCE_DIR}/cmake/lib/src)
-
 # Conan package manager
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
   message(
@@ -30,7 +12,7 @@ if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     "${CMAKE_BINARY_DIR}/conan.cmake")
 endif()
 
-include(${CMAKE_BINARY_DIR}/conan.cmake)
+include(conan)
 
 conan_cmake_run(
   CONANFILE
@@ -42,4 +24,3 @@ conan_cmake_run(
   missing)
 
 include(CTest)
-include(me_build_api)
